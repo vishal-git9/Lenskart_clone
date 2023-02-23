31262191937c01dcf55de9e6e76e733bf39152f9
Ankitkm95 <ankitmishra517@gmail.com>
2023-02-23 23:42:00 +0530

    loginSignup

--- lenskartapp/src/App.js
+++ lenskartapp/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import MidOne from './AfterNavbar/MidOne';
 import './App.css';
+import LoginComponent from './Login.jsx/LoginComponent';
+import SignUpComponent from './Login.jsx/SingnUpComponent';
 
 function App() {
   return (
     <div className="App">
       <MidOne />
+      <LoginComponent />
+      <SignUpComponent />
     </div>
   );
 }
---
+++ lenskartapp/src/Login.jsx/LoginComponent.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    useDisclosure,
+    Image
+  } from '@chakra-ui/react';
+
+
+const LoginComponent = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const initialRef = React.useRef(null)
+  const finalRef = React.useRef(null)
+
+  return (
+    <>
+      <Button onClick={onOpen}>Login</Button>
+      {/* <Button ml={4} ref={finalRef}>
+        I'll receive focus on close
+      </Button> */}
+        
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        >
+        
+        <ModalOverlay />
+        <ModalContent>
+            <Image src='https://static1.lenskart.com/media/desktop/img/DesignStudioIcons/DesktopLoginImage.svg' />
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>First name</FormLabel>
+              <Input ref={initialRef} placeholder='First name' />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Last name</FormLabel>
+              <Input placeholder='Last name' />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      
+    </>
+  )
+};
+
+export default LoginComponent;
